44e01fa2b156718f5bcdfa2080851c67bda16e8b
ip2whois <support@ip2whois.com>
2024-11-05 07:33:59 +0800

    Minor updates
    - Added new functions
    - Update DESCRIPTION file.

--- R/IP2whois.R
+++ R/IP2whois.R
@@ -2,7 +2,7 @@
 #'
 #' @description Set IP2Location.io API key for lookup. Free API key can be obtained from <https://www.ip2location.io/sign-up?ref=1/>
 #' @param api_key IP2Location.io API key
-#' @return NULL
+#' @return No return value, called for side effects.
 #' @import reticulate
 #' @export
 #' @examples \dontrun{
@@ -37,6 +37,147 @@ lookup <- function(domain){
   result = fromJSON(py$j)
   return(result)
 }
+
+#' @title Lookup for registrar information
+#'
+#' @description Lookup for registrar information
+#' @param domain domain name to lookup for
+#' @return Return the registrar information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupRegistrar("example.com")
+#' }
+#'
+
+lookupRegistrar <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("registrar = rec['registrar']")
+  result_from_python <- py$registrar
+  return(result_from_python)
+}
+
+#' @title Lookup for registrant information
+#'
+#' @description Lookup for registrant information
+#' @param domain domain name to lookup for
+#' @return Return the registrant information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupRegistrant("example.com")
+#' }
+#'
+
+lookupRegistrant <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("registrant = rec['registrant']")
+  result_from_python <- py$registrant
+  return(result_from_python)
+}
+
+#' @title Lookup for admin information
+#'
+#' @description Lookup for admin information
+#' @param domain domain name to lookup for
+#' @return Return the admin information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupAdmin("example.com")
+#' }
+#'
+
+lookupAdmin <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("admin = rec['admin']")
+  result_from_python <- py$admin
+  return(result_from_python)
+}
+
+#' @title Lookup for tech information
+#'
+#' @description Lookup for tech information
+#' @param domain domain name to lookup for
+#' @return Return the tech information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupTech("example.com")
+#' }
+#'
+
+lookupTech <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("tech = rec['tech']")
+  result_from_python <- py$tech
+  return(result_from_python)
+}
+
+#' @title Lookup for billing information
+#'
+#' @description Lookup for billing information
+#' @param domain domain name to lookup for
+#' @return Return the billing information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupBilling("example.com")
+#' }
+#'
+
+lookupBilling <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("billing = rec['billing']")
+  result_from_python <- py$billing
+  return(result_from_python)
+}
+
+#' @title Lookup for nameservers information
+#'
+#' @description Lookup for nameservers information
+#' @param domain domain name to lookup for
+#' @return Return the nameservers information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupNameservers("example.com")
+#' }
+#'
+
+lookupNameservers <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("nameservers = rec['nameservers']")
+  result_from_python <- py$nameservers
+  return(result_from_python)
+}
+
+#' @title Lookup for whois server information
+#'
+#' @description Lookup for whois server information
+#' @param domain domain name to lookup for
+#' @return Return the whois server information of the domain
+#' @import reticulate
+#' @export
+#' @examples \dontrun{
+#' lookupWhoisServer("example.com")
+#' }
+#'
+
+lookupWhoisServer <- function(domain){
+  address = paste("rec = ip2whois_init.lookup('", domain, "')", sep = "")
+  py_run_string(address)
+  py_run_string("whois_server = rec['whois_server']")
+  result_from_python <- py$whois_server
+  return(result_from_python)
+}
+
 
 #' @title Get Punycode for domain name
 #'
